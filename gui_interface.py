f5745a7b023e059b9f720d9e1f2b1172153e2735
Eren <julian07027@gmail.com>
2020-08-05 22:24:47 +0800

    update vscode setting, and gui script

--- gui_interface.py
+++ gui_interface.py
@@ -5,7 +5,7 @@ import os
 import sys
 import Pmw
 import tkinter as tk
-# from argparse import ArgumentParser
+from argparse import ArgumentParser
 
 
 class msg:
@@ -49,7 +49,7 @@ class Application(tk.Frame):
         self.master.resizable(width=0, height=0)
         # self.master.configure(bg='grey')
 
-        #NOTE: GUI menu
+        #NOTE: --- GUI menu
         self.menu = tk.Menu(self.master)
         self.filemenu = tk.Menu(self.menu, tearoff=False)
         self.menu.add_cascade(menu=self.filemenu, label='File')
@@ -57,7 +57,7 @@ class Application(tk.Frame):
         self.filemenu.add_command(label='Exit', command=self.master.quit)
         self.master.config(menu=self.menu)
 
-        #NOTE: Memory information
+        #NOTE: --- Memory information
         self.process = ""
         self.compiler = ""
         self.word = ""
@@ -66,33 +66,31 @@ class Application(tk.Frame):
         self.instance = ""
         self.pvt = ""
         self.bigdata = ""
-        self.proc_dict = {
+        self.proc_cmp_dict = {
             'n5': ["n5compilerA", "n5compilerB"],
             'n7': ["n7compilerA", "n7compilerB"]
         }
-        self.pvt_dict = dict()
-        self.pvt_dict = {
-            'n5compilerA_2KX4_B256M8': ['pvt1', 'pvt2'],
-            'n5compilerA_2KX4_B512M8': ['pvt1', 'pvt2']
-        }
+        self.inst_pvt_dict = dict()
 
-        #NOTE: [1] Compiler selection frame
+
+        #NOTE: --- Draw the GUI interface
+        #NOTE: [1] Selection : Compiler selection frame
         self.sel_frame = tk.LabelFrame(self.master, text='1. Compiler Selection')
         self.sel_frame.grid(row=0, column=0, sticky='NW')
 
         self.proc_var = tk.StringVar()        
         self.proc_label = tk.Label(self.sel_frame, text="Process: ")
-        self.proc_label.grid(row=0, column=0, sticky='E')
-        self.proc_opt = tk.OptionMenu(self.sel_frame, self.proc_var, *self.proc_dict.keys())
+        self.proc_label.grid(row=0, column=0, padx=3, pady=3, sticky='E')
+        self.proc_opt = tk.OptionMenu(self.sel_frame, self.proc_var, *self.proc_cmp_dict.keys())
         self.proc_opt.grid(row=0, column=1, sticky='W')
 
         self.cmp_var = tk.StringVar()
         self.cmp_label = tk.Label(self.sel_frame, text="Compiler: ")
-        self.cmp_label.grid(row=1, column=0, sticky='E')
+        self.cmp_label.grid(row=1, column=0, padx=3, pady=3, sticky='E')
         self.cmp_opt = tk.OptionMenu(self.sel_frame, self.cmp_var, '')
         self.cmp_opt.grid(row=1, column=1, sticky='W')
 
-        #NOTE: [2] Configuration frame
+        #NOTE: [2] Selection : Configuration frame
         self.cfg_frame = tk.LabelFrame(self.master, text='2. Configuration')
         self.cfg_frame.grid(row=0, column=1, sticky='NW')
 
@@ -114,47 +112,122 @@ class Application(tk.Frame):
         self.bw_opt = tk.OptionMenu(self.cfg_frame, self.bw_var, *self.bw_list)
         self.bw_opt.grid(row=2, column=1)
 
-        #NOTE: Click button frame
+        #NOTE: Click button frame -> return instance from database
         self.click_frame = tk.LabelFrame(self.master, text='')
         self.click_frame.grid(row=2, column=0, columnspan=2)
-        self.cfg_submit = tk.Button(self.click_frame, text='Click', command=self.save_select, borderwidth=3)
+        self.cfg_submit = tk.Button(self.click_frame, text='Click', command=self.query_instance, borderwidth=3)
         self.cfg_submit.grid(row=0, column=0, columnspan=2)
 
-        ## set default process to option menu
-        default_proc = list(self.proc_dict.keys())[0]
-        default_compiler = list(self.proc_dict[default_proc])[0]
-        self.proc_var.set(default_proc)
-        self.cmp_var.set(default_compiler)
-        ## trace process variable when user select
-        self.proc_var.trace_add('write', self.update_cmp_menu)
+        #NOTE: [3] Preview : Instance frame
+        self.prev_frame = tk.LabelFrame(self.master, text='3. Preview')
+        self.prev_frame.grid(row=3, column=0, sticky='NW')
 
+        self.inst_var = tk.StringVar()
+        self.inst_label = tk.Label(self.prev_frame, text="    Selected Instance: ")
+        self.inst_label.grid(row=0, column=0, sticky='W')
+        self.inst_opt = tk.Radiobutton(self.prev_frame, variable=self.inst_var, text='', value='')
+        self.inst_opt.grid(row=1, column=0, sticky='W')
+
+        #NOTE: [4] Preview : PVT selection frame
+        self.pvt_frame = tk.LabelFrame(self.master, text='4. PVT')
+        self.pvt_frame.grid(row=3, column=1, sticky='NW')
+
+        self.pvt_var = tk.StringVar()
+        self.pvt_label = tk.Label(self.pvt_frame, text="PVT: ")
+        self.pvt_label.grid(row=0, column=0, sticky='E')
+        self.pvt_opt = tk.OptionMenu(self.pvt_frame, self.pvt_var, '')
+        self.pvt_opt.grid(row=0, column=1, sticky='W')
+
+        #NOTE: [5] PPA : timing frame
+        self.timing_frame = tk.LabelFrame(self.master, text='Timing')
+        self.timing_frame.grid(row=5, column=0, sticky='NW')
+
+        #NOTE: [6] PPA : power frame
+        self.power_frame = tk.LabelFrame(self.master, text='Power')
+        self.power_frame.grid(row=5, column=1, sticky='NW')
+
+
+        #NOTE: --- Set default value, and trace user selection
+        ## set default value
+        default_proc = list(self.proc_cmp_dict.keys())[0]
+        self.proc_var.set(default_proc)
+        default_compiler = list(self.proc_cmp_dict[default_proc])[0]
+        self.cmp_var.set(default_compiler)
+
+        ## After click button -> update instance list
+        self.inst_pvt_dict = {
+            'n5compilerA_2KX4_B256M8': ['pvt1', 'pvt2'],
+            'n5compilerA_2KX4_B512M8': ['pvt3', 'pvt4'],
+            'n5compilerA_2KX4_B64M4': ['pvt5', 'pvt6']
+        }
+        self.inst_list = list(self.inst_pvt_dict.keys())
+        # self.inst_var.set(self.inst_list[0])
+        # for num, inst in enumerate(self.inst_list):
+        #     self.inst_opt = tk.Radiobutton(self.prev_frame,
+        #                                         variable=self.inst_var,
+        #                                         text=inst,
+        #                                         value=inst)
+        #     self.inst_opt.grid(row=num, column=1, sticky='W')
+
+        # pvt_list = self.inst_pvt_dict[self.inst_var.get()]
+        # self.pvt_var.set(pvt_list[0])
+        
+        ## trace value by user select
+        self.proc_var.trace_add('write', self.update_cmp_menu)
+        self.inst_var.trace_add('write', self.update_pvt_menu)
+
+        ###
         # print("Waiting ...")
         # self.proc_opt.wait_variable(self.proc_var)
         # print("Done waiting.")
+        ###
+
 
     def update_cmp_menu(self, *args):
         """
         Based on process selection to update compiler list
         ref: https://stackoverflow.com/questions/17252096/change-optionmenu-based-on-what-is-selected-in-another-optionmenu
         """
-        cmp_list = self.proc_dict[self.proc_var.get()]
+        cmp_list = self.proc_cmp_dict[self.proc_var.get()]
         self.cmp_var.set(cmp_list[0])
         menu = self.cmp_opt['menu']
         menu.delete(0, 'end')
         for cmp in cmp_list:
             menu.add_command(label=cmp, command=lambda nation=cmp: self.cmp_var.set(nation))
 
-    def save_select(self):
-        return (overwrite_var(self.proc_var.get(), self.process),
-                overwrite_var(self.cmp_var.get(), self.compiler),
-                overwrite_var(self.word_entry.get(), self.word),
-                overwrite_var(self.bit_entry.get(), self.bit),
-                overwrite_var(self.bw_var.get(), self.bytewrite))
+    def update_pvt_menu(self, *args):
+        """
+        Based on instance selection to update pvt list
+        """
+        pvt_list = self.inst_pvt_dict[self.inst_var.get()]
+        self.pvt_var.set(pvt_list[0])
+        menu = self.pvt_opt['menu']
+        menu.delete(0, 'end')
+        for pvt in pvt_list:
+            menu.add_command(label=pvt, command=lambda nation=pvt: self.pvt_var.set(nation))
 
+    def query_instance(self):
+        self.process = self.proc_var.get()
+        self.compiler = self.cmp_var.get()
+        self.word = self.word_entry.get()
+        self.bit = self.bit_entry.get()
+        self.bytewrite = self.bw_var.get()
+        self.bigdata = 'bigdata path'
+        self.instance = ["n5compilerA", "n5compilerB"]
+        # for inst in self.instance:
+        #     self.inst_pvt_dict[inst] = ["pvvt1", "pvvt2"]
+        #     print(f'{inst}\n    {self.inst_pvt_dict[inst]}')
+        self.create_instance_radiobutton(self.instance)
 
-def overwrite_var(selection, global_var):
-    global_var = selection
-    print(global_var)
+    def create_instance_radiobutton(self, inst_list):
+        ## Remove original radiobutton
+        for radio_opt in self.prev_frame.grid_slaves():
+            if int(radio_opt.grid_info()["row"]) > 0:
+                radio_opt.grid_forget()
+        ## Create selected instance radiobutton
+        for num, inst in enumerate(self.inst_list):
+            self.inst_opt = tk.Radiobutton(self.prev_frame, variable=self.inst_var, text=inst, value=inst)
+            self.inst_opt.grid(row=num+1, column=0, sticky='W')
 
 
 if __name__ == '__main__':
