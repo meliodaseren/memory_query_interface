0127465f9955bb1e5b8be3375e52d770118ceb12
Eren <julian07027@gmail.com>
2020-08-07 18:04:46 +0800

    update sample golden file

--- gui_interface.py
+++ gui_interface.py
@@ -3,9 +3,11 @@
 import re
 import os
 import sys
-import Pmw
+import json
+# import Pmw
 import tkinter as tk
-from argparse import ArgumentParser
+import tkinter.font as tkFont
+# from argparse import ArgumentParser
 
 
 class msg:
@@ -37,17 +39,31 @@ class msg:
         self.ENDC = ''
         self.BOLD = ''
         self.UNDERLINE = ''
-# print(msg.info, msg.ok, msg.warn, msg.error, msg.debug, msg.trace)
+
+
+def load_json(control_json):
+    with open(control_json) as f:
+        menu = json.load(f)
+    return menu
 
 
 class Application(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
         self.master = master
-        self.master.title('Memory Query')
-        self.master.geometry('640x480')
-        self.master.resizable(width=0, height=0)
+        self.master.title('Memory PPA Query')
+        self.master.geometry('1024x768')
+        self.master.minsize(640, 768)
+        # self.master.resizable(width=0, height=0)
         # self.master.configure(bg='grey')
+
+        self.master.columnconfigure(0, weight=1)
+        self.master.columnconfigure(1, weight=1)
+        self.master.rowconfigure(4, weight=1)
+
+        self.title_font_stype = tkFont.Font(family= 'Ubuntu Light', size=14, weight='bold')
+        self.frame_font_stype = tkFont.Font(family= 'Ubuntu Light', size=12, weight='normal')
+        self.label_font_stype = tkFont.Font(family= 'Ubuntu Light', size=10, weight='normal')
 
         #NOTE: --- GUI menu
         self.menu = tk.Menu(self.master)
@@ -66,87 +82,103 @@ class Application(tk.Frame):
         self.instance = ""
         self.pvt = ""
         self.bigdata = ""
-        self.proc_cmp_dict = {
-            'n5': ["n5compilerA", "n5compilerB"],
-            'n7': ["n7compilerA", "n7compilerB"]
-        }
-        self.inst_pvt_dict = dict()
 
+        self.menu_dict = self.load_proc_cmp_menu()
+        self.proc_cmp_dict = dict()
+        self.update_proc_cmp_dict(self.menu_dict)
+        self.inst_pvt_dict = dict()
+        self.pvt_list = ""
 
         #NOTE: --- Draw the GUI interface
+        self.title_frame = tk.Label(self.master, text='Memory PPA Query', font=self.title_font_stype)
+        self.title_frame.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
+
         #NOTE: [1] Selection : Compiler selection frame
-        self.sel_frame = tk.LabelFrame(self.master, text='1. Compiler Selection')
-        self.sel_frame.grid(row=0, column=0, sticky='NW')
+        self.sel_frame = tk.LabelFrame(self.master, text='1. Compiler Selection', font=self.frame_font_stype)
+        self.sel_frame.grid(row=1, column=0, sticky='NEWS')
 
-        self.proc_var = tk.StringVar()        
-        self.proc_label = tk.Label(self.sel_frame, text="Process: ")
-        self.proc_label.grid(row=0, column=0, padx=3, pady=3, sticky='E')
-        self.proc_opt = tk.OptionMenu(self.sel_frame, self.proc_var, *self.proc_cmp_dict.keys())
-        self.proc_opt.grid(row=0, column=1, sticky='W')
+        #NOTE: [1-1] Process option menu
+        self.proc_var = tk.StringVar()
+        self.proc_label = tk.Label(self.sel_frame, text="Process: ", font=self.label_font_stype)
+        self.proc_label.grid(row=0, column=0, padx=5, pady=0, sticky='E')
+        self.proc_opt = tk.OptionMenu(self.sel_frame, self.proc_var, *self.menu_dict.keys())
+        self.proc_opt.grid(row=0, column=1, padx=5, pady=0, sticky='W')
 
+        #NOTE: [1-2] Compiler option menu
         self.cmp_var = tk.StringVar()
-        self.cmp_label = tk.Label(self.sel_frame, text="Compiler: ")
-        self.cmp_label.grid(row=1, column=0, padx=3, pady=3, sticky='E')
+        self.cmp_label = tk.Label(self.sel_frame, text="Compiler: ", font=self.label_font_stype)
+        self.cmp_label.grid(row=1, column=0, padx=5, pady=0, sticky='E')
         self.cmp_opt = tk.OptionMenu(self.sel_frame, self.cmp_var, '')
-        self.cmp_opt.grid(row=1, column=1, sticky='W')
+        self.cmp_opt.grid(row=1, column=1, padx=5, pady=0, sticky='W')
 
         #NOTE: [2] Selection : Configuration frame
-        self.cfg_frame = tk.LabelFrame(self.master, text='2. Configuration')
-        self.cfg_frame.grid(row=0, column=1, sticky='NW')
+        self.cfg_frame = tk.LabelFrame(self.master, text='2. Configuration', font=self.frame_font_stype)
+        self.cfg_frame.grid(row=1, column=1, sticky='NEWS')
 
-        self.word_label = tk.Label(self.cfg_frame, text="Word: ")
-        self.word_label.grid(row=0, column=0, sticky='E')
+        self.word_label = tk.Label(self.cfg_frame, text="Word: ", font=self.label_font_stype)
+        self.word_label.grid(row=0, column=0, padx=5, pady=0, sticky='E')
         self.word_entry = tk.Entry(self.cfg_frame)
-        self.word_entry.grid(row=0, column=1, sticky='W')
+        self.word_entry.grid(row=0, column=1, padx=5, pady=0, sticky='W')
 
-        self.bit_label = tk.Label(self.cfg_frame, text="Bit: ")
-        self.bit_label.grid(row=1, column=0, sticky='E')
+        self.bit_label = tk.Label(self.cfg_frame, text="Bit: ", font=self.label_font_stype)
+        self.bit_label.grid(row=1, column=0, padx=5, pady=0, sticky='E')
         self.bit_entry = tk.Entry(self.cfg_frame)
-        self.bit_entry.grid(row=1, column=1, sticky='W')
+        self.bit_entry.grid(row=1, column=1, padx=5, pady=0, sticky='W')
 
         self.bw_list = ("0", "1")
         self.bw_var = tk.StringVar()
         self.bw_var.set(self.bw_list[0])
-        self.bw_label = tk.Label(self.cfg_frame, text="Byte Write: ")
-        self.bw_label.grid(row=2, column=0, sticky='E')
+        self.bw_label = tk.Label(self.cfg_frame, text="Byte Write: ", font=self.label_font_stype)
+        self.bw_label.grid(row=2, column=0, padx=5, pady=0, sticky='E')
         self.bw_opt = tk.OptionMenu(self.cfg_frame, self.bw_var, *self.bw_list)
-        self.bw_opt.grid(row=2, column=1)
+        self.bw_opt.grid(row=2, column=1, padx=5, pady=0, sticky='W')
 
         #NOTE: Click button frame -> return instance from database
-        self.click_frame = tk.LabelFrame(self.master, text='')
-        self.click_frame.grid(row=2, column=0, columnspan=2)
-        self.cfg_submit = tk.Button(self.click_frame, text='Click', command=self.query_instance, borderwidth=3)
-        self.cfg_submit.grid(row=0, column=0, columnspan=2)
+        self.click_frame = tk.LabelFrame(self.master, text='3. Select Instance', font=self.frame_font_stype)
+        self.click_frame.grid(row=2, column=0, columnspan=2, sticky='NEWS')
+        self.click_submit = tk.Button(self.click_frame,
+                                           text='Click',
+                                           command=self.query_instance,
+                                           borderwidth=3,
+                                           font=self.label_font_stype)
+        self.click_submit.pack(side='top', fill='x')
 
         #NOTE: [3] Preview : Instance frame
-        self.prev_frame = tk.LabelFrame(self.master, text='3. Preview')
-        self.prev_frame.grid(row=3, column=0, sticky='NW')
+        self.prev_frame = tk.LabelFrame(self.master, text='4. Preview Instance', font=self.frame_font_stype)
+        self.prev_frame.grid(row=3, column=0, sticky='NEWS')
 
+        #NOTE: [3-1] Instance list
         self.inst_var = tk.StringVar()
-        self.inst_label = tk.Label(self.prev_frame, text="    Selected Instance: ")
-        self.inst_label.grid(row=0, column=0, sticky='W')
+        self.inst_label = tk.Label(self.prev_frame, text="    Selected Instance: ", font=self.label_font_stype)
+        self.inst_label.grid(row=0, column=0, padx=5, pady=0, sticky='W')
         self.inst_opt = tk.Radiobutton(self.prev_frame, variable=self.inst_var, text='', value='')
-        self.inst_opt.grid(row=1, column=0, sticky='W')
+        self.inst_opt.grid(row=1, column=0, padx=5, pady=0, sticky='W')
 
         #NOTE: [4] Preview : PVT selection frame
-        self.pvt_frame = tk.LabelFrame(self.master, text='4. PVT')
-        self.pvt_frame.grid(row=3, column=1, sticky='NW')
+        self.pvt_frame = tk.LabelFrame(self.master, text='5. PVT', font=self.frame_font_stype)
+        self.pvt_frame.grid(row=3, column=1, sticky='NEWS')
 
+        #NOTE: [4-1] PVT list
         self.pvt_var = tk.StringVar()
-        self.pvt_label = tk.Label(self.pvt_frame, text="PVT: ")
-        self.pvt_label.grid(row=0, column=0, sticky='E')
-        self.pvt_opt = tk.OptionMenu(self.pvt_frame, self.pvt_var, '')
-        self.pvt_opt.grid(row=0, column=1, sticky='W')
+        self.pvt_label = tk.Label(self.pvt_frame, text="PVT List: ", font=self.label_font_stype)
+        self.pvt_label.pack(side='left')
+        self.pvt_entry = tk.Entry(self.pvt_frame, textvariable=self.pvt_var, width=10)
+        self.pvt_entry.pack(side='top', fill='x')
+        self.pvt_scrollbar = tk.Scrollbar(self.pvt_frame)
+        self.pvt_scrollbar.pack(side='right', fill='y')
+        self.pvt_listbox = tk.Listbox(self.pvt_frame, yscrollcommand=self.pvt_scrollbar.set)
+        self.pvt_listbox.pack(side='top', fill='both')
+        self.pvt_scrollbar.config(command=self.pvt_listbox.yview)
 
         #NOTE: [5] PPA : timing frame
-        self.timing_frame = tk.LabelFrame(self.master, text='Timing')
-        self.timing_frame.grid(row=5, column=0, sticky='NW')
+        self.timing_frame = tk.LabelFrame(self.master, text='6. Timing', font=self.frame_font_stype)
+        self.timing_frame.grid(row=4, column=0, sticky='NEWS')
         self.timing_label = tk.Label(self.timing_frame, text="Timing: ")
         self.timing_label.grid(row=0, column=0, sticky='W')
 
         #NOTE: [6] PPA : power frame
-        self.power_frame = tk.LabelFrame(self.master, text='Power')
-        self.power_frame.grid(row=5, column=1, sticky='NW')
+        self.power_frame = tk.LabelFrame(self.master, text='7. Power', font=self.frame_font_stype)
+        self.power_frame.grid(row=4, column=1, sticky='NEWS')
         self.power_label = tk.Label(self.power_frame, text='Power: ')
         self.power_label.grid(row=0, column=0, sticky='W')
 
@@ -160,40 +192,43 @@ class Application(tk.Frame):
         ## trace value by user select
         self.proc_var.trace_add('write', self.update_cmp_menu)
         self.inst_var.trace_add('write', self.update_pvt_menu)
+        self.pvt_listbox.bind("<<ListboxSelect>>", self.query_ppa)
 
-        ###
+        ### ----------------------------------------------------------------- ###
+        # TODO: Waiting
         # print("Waiting ...")
         # self.proc_opt.wait_variable(self.proc_var)
         # print("Done waiting.")
-        ###
+        ### ----------------------------------------------------------------- ###
 
+    def load_proc_cmp_menu(self, *args):
+        control_json = 'menu.json'
+        menu_dict = load_json(control_json)
+        return menu_dict
 
-    def update_cmp_menu(self, *args):
+    def update_proc_cmp_dict(self, menu, *args):
         """
-        Based on process selection to update compiler list
-        ref: https://stackoverflow.com/questions/17252096/change-optionmenu-based-on-what-is-selected-in-another-optionmenu
+        Load compiler list to list, and update to dictionary
         """
-        cmp_list = self.proc_cmp_dict[self.proc_var.get()]
-        self.cmp_var.set(cmp_list[0])
-        menu = self.cmp_opt['menu']
-        menu.delete(0, 'end')
-        for cmp in cmp_list:
-            menu.add_command(label=cmp, command=lambda nation=cmp: self.cmp_var.set(nation))
+        for process in menu.keys():
+            cmplist_path = menu[process]['cmp_list']
+            with open(cmplist_path, 'r') as f:
+                cmp_list = [cmp for cmp in map(str.strip, f)]
+            self.proc_cmp_dict[process] = cmp_list
 
-    def update_pvt_menu(self, *args):
-        """
-        Based on instance selection to update pvt list
-        """
-        #HACK: hacking instance-pvt dictionary
-        self.inst_pvt_dict[self.inst_var.get()] = ["pvvt1", "pvvt2"]
-        print(f'{msg.debug} {self.inst_var.get()} {self.inst_pvt_dict[self.inst_var.get()]}')
+    def remove_instance_radiobutton(self, *args):
+        ## Remove original radiobutton
+        ## https://stackoverflow.com/questions/23189610/remove-widgets-from-grid-in-tkinter
+        self.instance = ""
+        for radio_opt in self.prev_frame.grid_slaves():
+            if int(radio_opt.grid_info()["row"]) > 0:
+                radio_opt.grid_forget()
 
-        pvt_list = self.inst_pvt_dict[self.inst_var.get()]
-        self.pvt_var.set(pvt_list[0])
-        menu = self.pvt_opt['menu']
-        menu.delete(0, 'end')
-        for pvt in pvt_list:
-            menu.add_command(label=pvt, command=lambda nation=pvt: self.pvt_var.set(nation))
+    def create_instance_radiobutton(self, inst_list, *args):
+        ## Create selected instance radiobutton
+        for num, inst in enumerate(inst_list):
+            self.inst_opt = tk.Radiobutton(self.prev_frame, variable=self.inst_var, text=inst, value=inst)
+            self.inst_opt.grid(row=num+1, column=0, sticky='W')
 
     def query_instance(self, *args):
         """
@@ -214,19 +249,37 @@ class Application(tk.Frame):
             self.instance = ["n7compilerA_2KX4_B256M8", "n7compilerA_2KX4_B512M8"]
         self.create_instance_radiobutton(self.instance)
 
-    def remove_instance_radiobutton(self, *args):
-        ## Remove original radiobutton
-        ## https://stackoverflow.com/questions/23189610/remove-widgets-from-grid-in-tkinter
-        self.instance = ""
-        for radio_opt in self.prev_frame.grid_slaves():
-            if int(radio_opt.grid_info()["row"]) > 0:
-                radio_opt.grid_forget()
+    def update_cmp_menu(self, *args):
+        """
+        Based on process selection to update compiler list
+        ref: https://stackoverflow.com/questions/17252096/change-optionmenu-based-on-what-is-selected-in-another-optionmenu
+        """
+        cmp_list = self.proc_cmp_dict[self.proc_var.get()]
+        self.cmp_var.set(cmp_list[0])
+        menu = self.cmp_opt['menu']
+        menu.delete(0, 'end')
+        for cmp in cmp_list:
+            menu.add_command(label=cmp, command=lambda nation=cmp: self.cmp_var.set(nation))
 
-    def create_instance_radiobutton(self, inst_list, *args):
-        ## Create selected instance radiobutton
-        for num, inst in enumerate(inst_list):
-            self.inst_opt = tk.Radiobutton(self.prev_frame, variable=self.inst_var, text=inst, value=inst)
-            self.inst_opt.grid(row=num+1, column=0, sticky='W')
+    def update_pvt_menu(self, *args):
+        """
+        Based on instance selection to update pvt list
+        """
+        #HACK: hacking pvt list
+        inst = self.inst_var.get()
+        self.inst_pvt_dict[inst] = ["pvvt1", "pvvt2"]
+        print(f'{msg.debug} selece instance {self.inst_var.get()} -> {self.inst_pvt_dict[self.inst_var.get()]}')
+        pvt_list = self.inst_pvt_dict[inst]
+        self.pvt_listbox.delete(0, 'end')
+        for pvt in pvt_list:
+            self.pvt_listbox.insert("end", pvt)
+        self.pvt_list = pvt_list
+
+    def query_ppa(self, *args):
+        inst = self.inst_var.get()
+        pvt_idx_tuple = self.pvt_listbox.curselection()
+        pvt = [self.pvt_list[int(pvt_idx)] for pvt_idx in pvt_idx_tuple][0]
+        print(f'{msg.debug} query PPA: instance = {inst}, pvt = {pvt}')
 
 
 if __name__ == '__main__':
